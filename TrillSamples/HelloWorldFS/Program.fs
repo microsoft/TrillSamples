b2559d7ccb2981abc96c8275f9633bfe50120962
Peter Shrosbree <petshr@microsoft.com>
2018-12-27 12:48:34 -0800

    Extend Trill mappings
    Add optional parameters to toStreamable
    Map some overloads to F# functions

--- TrillSamples/HelloWorldFS/Program.fs
+++ TrillSamples/HelloWorldFS/Program.fs
@@ -4,34 +4,83 @@ open System.Linq.Expressions
 open System.Reactive.Linq
 open Microsoft.StreamProcessing
 
+open System.Runtime.CompilerServices
+
+[<Extension>]
+type Observable with
+    [<Extension>]
+    static member inline toStreamable(observable: IObservable<StreamEvent<'a>>, 
+                                      ?disorderPolicy : DisorderPolicy, 
+                                      ?flushPolicy : FlushPolicy, 
+                                      ?periodicPunctuationPolicy : PeriodicPunctuationPolicy,
+                                      ?onCompletedPolicy : OnCompletedPolicy) =
+        let disorderPolicy = DisorderPolicy.Throw() |> defaultArg disorderPolicy
+        let flushPolicy = FlushPolicy.FlushOnPunctuation |> defaultArg flushPolicy 
+        let periodicPunctuationPolicy = PeriodicPunctuationPolicy.None() |> defaultArg periodicPunctuationPolicy
+        let onCompletedPolicy = OnCompletedPolicy.EndOfStream |> defaultArg onCompletedPolicy
+        Streamable.ToStreamable(
+                    observable, 
+                    disorderPolicy, 
+                    flushPolicy, 
+                    periodicPunctuationPolicy, 
+                    onCompletedPolicy)
+    
 module Streamable =
     open Microsoft.StreamProcessing
-
-    let ofObservable observable =
-        Streamable.ToStreamable(
-            observable, 
-            DisorderPolicy.Throw(), 
-            FlushPolicy.FlushOnPunctuation, 
-            PeriodicPunctuationPolicy.None(), 
-            OnCompletedPolicy.EndOfStream)
             
-    let join mapping left right =
-        Streamable.Join(left, right, fun a b -> mapping a b)
+    let join map left right =
+        Streamable.Join(left, right, fun a b -> map a b)
         
-    let multicast (mapping:IStreamable<'k,'a> -> IStreamable<'k,'b>) source =
-        Streamable.Multicast(source, mapping)
+    let keyJoin map left right =
+        Streamable.Join(fst left, fst right, (fun a -> a |> snd left), (fun b -> b |> snd right), (fun a b -> map a b))
         
-    let toStreamEventObservable (source:IStreamable<Empty,'a>) =
+    let hintedKeyJoin options map left right =
+        Streamable.Join(fst left, fst right, (fun a -> a |> snd left), (fun b -> b |> snd right), (fun a b -> map a b), options)
+        
+    let multicast (map:IStreamable<'TKey,'TPayload> -> IStreamable<'TKey,'TResult>) source =
+        Streamable.Multicast(source, map)
+        
+    let multicastMerge (map:IStreamable<'k,'a> -> IStreamable<'k,'b> -> IStreamable<'k,'c>) sourceLeft sourceRight =
+        Streamable.Multicast(sourceLeft, sourceRight, fun a b -> map a b)
+        
+    let multicastCount (count:int) source : IStreamable<'TKey,'TPayload>[] =
+        Streamable.Multicast(source, count)
+        
+    let toStreamEventObservable (source:IStreamable<Empty,'TPayload>) =
         Streamable.ToStreamEventObservable(source)
         
-    let alterEventLifetime (duration:int64) startTimeSelector source =
+    let toReshapedStreamEventObservable reshapingPolicy (source:IStreamable<Empty,'TPayload>) =
+        Streamable.ToStreamEventObservable(source, reshapingPolicy)
+        
+    let toPartitionedStreamEventObservable (source:IStreamable<PartitionKey<'TKey>,'TPayload>) =
+        Streamable.ToStreamEventObservable(source)
+        
+    let toReshapedPartitionedStreamEventObservable reshapingPolicy (source:IStreamable<PartitionKey<'TKey>,'TPayload>) =
+        Streamable.ToStreamEventObservable(source, reshapingPolicy)
+        
+    let setEventLifetime startTimeSelector (duration:int64) source =
         Streamable.AlterEventLifetime(source, (fun x -> startTimeSelector x), duration)
         
-    let filter predicate source =
+    let mapEventLifetime startTimeSelector (map:int64 -> int64) source =
+        Streamable.AlterEventLifetime(source, (fun x -> startTimeSelector x), fun duration -> map duration)
+        
+    let mergeEventLifetime startTimeSelector (merge:int64 -> int64 -> int64) source =
+        Streamable.AlterEventLifetime(source, (fun x -> startTimeSelector x), fun a b -> merge a b)
+        
+    let mergePartitionedEventLifetime startTimeSelector (merge:'TPartition -> int64 -> int64 -> int64) source : IStreamable<PartitionKey<'TPartition>,'TPayload> =
+        Streamable.AlterEventLifetime(source, (fun p x -> startTimeSelector p x), fun a b -> merge a b)
+        
+    let filter predicate source : IStreamable<'TKey,'TPayload> =
         Streamable.Where(source, fun e -> predicate e)
         
-    let map mapping source =
-        Streamable.Select(source, fun e -> mapping e)
+    let map (map:'TPayload->'TResult) source : IStreamable<'TKey,'TResult>=
+        Streamable.Select(source, fun e -> map e)
+
+    let bind (map:'TLeft -> 'TRight -> 'TResult) (left:IStreamable<'TKey,'TLeft>) (right:Empty -> IStreamable<'TKey,'TRight>) =
+        Streamable.SelectMany(left, right, fun a b -> map a b)
+
+    let groupBind (groupDefinition:IMapDefinition<'TOuterKey,'TPayload,'TPayload,'TInnerKey,'TResult>) apply (resultSelector:GroupSelectorInput<'TInnerKey> -> 'TBind -> 'TOutput) =
+        Streamable.SelectMany(groupDefinition, apply, fun i b -> resultSelector i b)
 
 type SensorRange = { Time: int64; Low: int; High: int }
 type SensorReading = { Time: int64; Value: int }
@@ -44,14 +93,14 @@ let historicStream =
     historicData
     |> Observable.ToObservable
     |> Observable.map (fun r -> StreamEvent.CreateInterval(r.Time, r.Time + 1L, r))
-    |> Streamable.ofObservable
+    |> Observable.toStreamable
     
 let createStream isRealTime =
     historicStream
     
 let ranges threshold input =
     let above = input |> Streamable.filter (fun s -> s.Value > threshold)
-    let below = input |> Streamable.alterEventLifetime 1L ((+) 1L) |> Streamable.filter (fun s -> s.Value < threshold)
+    let below = input |> Streamable.setEventLifetime ((+) 1L) 1L |> Streamable.filter (fun s -> s.Value < threshold)
     let (|><|) = Streamable.join (fun a b -> { Time = a.Time; Low = b.Value; High = a.Value })
     below |><| above
 
